e704f66a8b5933622d78b0d434b7dc3b22466d35
igordevnet <igorsouzaalmeida2404@gmail.com>
2026-08-14 22:34:24 -0300

    Exercise proposed by Dheeru Mundluru in Understand JVM internals like Class Loading, Reflection, Garbage Collection, Heap & Stack and Bytecode Instruction Set

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+import java.lang.reflect.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        var result = execute(Reflection.class);
+
+        if (result == 2)
+            System.out.println("Congrats! You got it: " + result);
+        else
+            System.out.println("Eh... You did your best: " + result);
+    }
+
+    public static int execute(Class clazz) throws Exception {
+        int failedCount = 0;
+        ExecutorService service = Executors.newFixedThreadPool(10);
+
+        Object instance = Class.forName(clazz.getName()).newInstance();
+
+        for (Method m : clazz.getDeclaredMethods()) {
+            if (m.getName().startsWith("test")) {
+                if (m.getGenericReturnType() == boolean.class) {
+                    Future<Integer> resultThread = service.submit(new Callable<Integer>() {
+                        @Override
+                        public Integer call () throws Exception {
+                            try {
+                                Object result = m.invoke(instance);
+
+                                if (!(boolean) result)
+                                    return 1;
+
+                                return 0;
+                            } catch (Exception e) {
+                                return 0;
+                            }
+                        }
+                    });
+
+                    failedCount += resultThread.get();
+                }
+            }
+        }
+
+        service.shutdownNow();
+
+        return failedCount;
+    }
+}
+
